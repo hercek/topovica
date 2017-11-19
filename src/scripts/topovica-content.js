6f9b74234601411cdf460aed1fe93a7885e65e36
Pirateninjawizard <adrianqsk@gmail.com>
2017-11-19 21:36:13 +0800

    switch to using evt.key from keydown event, cos this will give modified values when shift is down

--- src/scripts/topovica-content.js
+++ src/scripts/topovica-content.js
@@ -5,45 +5,59 @@
  * as next. Hope this approach isn't too inefficient.
  */
 (function(){
+	if(window.hasRun) return;
+	window.hasRun = true;
+
 	var DEBUG = true;
 	var modes = {"command":0,"insert":1}, mode = modes.command;
 
 	// focused element
 	var focused = window;
 
-    // movement function. "by" is an array specifying x and y offsets to scroll by
-    function move(by){
-        focused.scrollBy.apply(focused, by);
-        return firstfn;
-    }
+	// movement function. "by" is an array specifying x and y offsets to scroll by
+	function move(by){
+		focused.scrollBy.apply(focused, by);
+		return firstfn;
+	}
 
-    // start chain functions
-    function firstfn(c){
-        // actions map
-        var actions = {
-            // basic movements
-            72: function(){ return move([-100,0]); },
-            74: function(){ return move([0,50]); },
-            75: function(){ return move([0,-50]); },
-            76: function(){ return move([100,0]); },
-            // gt, gT, g$, g^, gg
-            71: function(){ return gunit; }
-        }
+	var firstfn = null,
+		gunit = null;
 
-        if(c in actions){
-            return actions[c]();
-        }
-        return firstfn;
-    }
+	// create a chain function
+	function chainlink(actions){
+		return function(c){
+			try{
+				if(c in actions){
+					return actions[c]();
+				}
+			} catch(e) {}
+			return firstfn;
+		};
+	}
 
-    // function dealing with "g" possible completions are "^", "$", "g", "t" and "T"
-    function gunit(c){
-        debug("gunit");
-        return firstfn;
-    }
+	// start chain functions
+	firstfn = chainlink({
+		// basic movements
+		"h": function(){ return move([-100,0]); }, // left
+		"j": function(){ return move([0,50]); }, // down
+		"k": function(){ return move([0,-50]); }, // up
+		"l": function(){ return move([100,0]); }, // right
+		// gt, gT, g$, g^, gg
+		"g": function(){ return gunit; }
+	});
+
+	// function dealing with "g" possible completions are "^", "$", "g", "t" and "T"
+	gunit = chainlink({
+		"g": function(){ window.scrollTo(0,0); return firstfn; },
+		"Shift": function(){ return gunit; }
+	});
+	// end chain functions
 
 	// holds current command
 	var next = firstfn;
+
+	// holds undo for current keydown mod
+	var kunext = null;
 
 	function debug(){
 		if(!DEBUG) return;
@@ -52,25 +66,26 @@
 
 	// keydown handler
 	function kd(evt){
-		debug(evt.keyCode);
+		debug(evt.key);
 
-		var c = evt.keyCode;
+		var c = evt.key;
 
 		// ESC resets immediately
-		if(c==27){
-            if(mode==modes.insert) mode = modes.command;
-            next = firstfn;
+		if(c=="Escape"){
+			if(mode==modes.insert) mode = modes.command;
+			next = firstfn;
+			kunext = null;
 			return;
 		}
 
 		// we only react if we're in command mode
 		if(mode!=modes.command) return;
 
-        next = next(c);
+		next = next(c);
 	}
 
-    // keyup handler
-    function ku(evt){ }
+	// keyup handler
+	function ku(evt){ }
 
 	window.addEventListener("keydown", kd)
 	window.addEventListener("keyup", ku)
