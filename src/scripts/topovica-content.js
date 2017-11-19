af92b8042f1ff04f17e6a06cd181eca237546b81
Pirateninjawizard <adrianqsk@gmail.com>
2017-11-19 17:29:35 +0800

    changed from lookup to weird function chaining + lookup approach that allows a unified approach to parsing keypresses. hopefully all this function passing doesn't turn out to be too inefficient.

--- src/scripts/topovica-content.js
+++ src/scripts/topovica-content.js
@@ -1,62 +1,77 @@
+/**
+ * So how we are going to do this is, next will hold the next function to be executed.
+ * All the chained functions should accept a keyCode argument and return next nextfn,
+ * optionally executing an action along the way. Invalid combinations will reset firstfn
+ * as next. Hope this approach isn't too inefficient.
+ */
 (function(){
 	var DEBUG = true;
 	var modes = {"command":0,"insert":1}, mode = modes.command;
-	// movement map
-	var movements = {
-		72: function(){ focused.scrollBy(-100,0); },
-		74: function(){ focused.scrollBy(0,50); },
-		75: function(){ focused.scrollBy(0,-50); },
-		76: function(){ focused.scrollBy(100,0); }
-	}
-
-	// holds current command
-	var cmdbuffer = Array();
 
 	// focused element
 	var focused = window;
+
+    // movement function. "by" is an array specifying x and y offsets to scroll by
+    function move(by){
+        focused.scrollBy.apply(focused, by);
+        return firstfn;
+    }
+
+    // start chain functions
+    function firstfn(c){
+        // actions map
+        var actions = {
+            // basic movements
+            72: function(){ return move([-100,0]); },
+            74: function(){ return move([0,50]); },
+            75: function(){ return move([0,-50]); },
+            76: function(){ return move([100,0]); },
+            // gt, gT, g$, g^, gg
+            71: function(){ return gunit; }
+        }
+
+        if(c in actions){
+            return actions[c]();
+        }
+        return firstfn;
+    }
+
+    // function dealing with "g" possible completions are "^", "$", "g", "t" and "T"
+    function gunit(c){
+        debug("gunit");
+        return firstfn;
+    }
+
+	// holds current command
+	var next = firstfn;
 
 	function debug(){
 		if(!DEBUG) return;
 		console.log.apply(undefined, arguments);
 	}
 
-	function clearbuf(){
-		cmdbuffer.splice(0,cmdbuffer.length)
-	}
-
-	// if buffer is in this state, execute command
-	function dispatch(){
-		// check for movements
-		if(cmdbuffer.length==1 && (cmdbuffer in movements)){
-			movements[cmdbuffer[0]]();
-			clearbuf();
-			return;
-		}
-	}
-
-	function toggle_mode(){
-		mode = mode==modes.command?modes.insert:modes.command;
-	}
-
 	// keydown handler
 	function kd(evt){
 		debug(evt.keyCode);
-		// we only react if we're in command mode
-		if(mode!=modes.command) return;
 
 		var c = evt.keyCode;
 
-		// ESC clears buffer immediately
+		// ESC resets immediately
 		if(c==27){
-			clearbuf()
+            if(mode==modes.insert) mode = modes.command;
+            next = firstfn;
 			return;
 		}
 
-		cmdbuffer.push(c);
-		debug(cmdbuffer);
-		dispatch();
+		// we only react if we're in command mode
+		if(mode!=modes.command) return;
+
+        next = next(c);
 	}
 
+    // keyup handler
+    function ku(evt){ }
+
 	window.addEventListener("keydown", kd)
-	
+	window.addEventListener("keyup", ku)
 })();
